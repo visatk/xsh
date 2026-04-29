4a8642cb93b176cc7bdd503acc6df5f05b61df65
visatk <visatk29@gmail.com>
2026-04-30 01:50:25 +0600

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,15 @@
 export interface Env {
   DB: D1Database;
-  BOT_TOKEN: string;
+  TG_BOT_TOKEN: string;
+  APIRONE_ACCOUNT: string;
+  ADMIN_TG_ID: string;
   WEBHOOK_SECRET: string;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price_usd: number;
+  available_stock: number;
 }
